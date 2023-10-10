80e14115e53e61a07e998484477c87b4370e89aa
janine <j.liebscher@bbqberlin.onmicrosoft.com>
2023-10-10 19:54:09 +0200

    Timer

---
+++ Timers/test.js
@@ -0,0 +1,33 @@
+var myMessage = document.getElementById("message");
+
+function showMessage(){
+    myMessage.className ="show";
+
+}
+
+setTimeout(showMessage, 3000); //milisecond = 3 seconds
+
+
+
+
+var colorChanger = document.getElementById("color-changer");
+var colors  =["red", "blue" ,"green", "pink"];
+var counter = 0;
+
+function changeColor(){
+
+    if(counter >= colors.length){
+        counter = 0;
+    }
+    colorChanger.style.background =colors[counter];
+    counter ++;
+
+}
+
+var myTimer = setInterval(changeColor, 3000);
+
+
+colorChanger.onclick = function(){
+    clearInterval(myTimer);
+    colorChanger.innerHTML = "Timer stopped";
+}
